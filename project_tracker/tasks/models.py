5fc49bd9ee1ce91835d0b621b1b950f6ee5b56db
Dmitrii <fedorovds@student.bmstu.ru>
2024-04-18 22:04:38 +0300

    init

---
+++ project_tracker/tasks/models.py
@@ -0,0 +1,115 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('New', 'Новая'),
+        ('In_progress', 'В работе'),
+        ('Completed', 'Завершена'),
+    ]
+
+    project = models.ForeignKey(
+        Project,
+        related_name='tasks',
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    assignee = models.ForeignKey(
+        User,
+        related_name='tasks',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='New',
+    )
+
+class BugReport(models.Model):
+    STATUS_CHOICES = [
+        ('New', 'Новая'),
+        ('In_progress', 'В работе'),
+        ('Completed', 'Завершена'),
+    ]
+    PRIORITY_CHOICES = [
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE
+    )
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='New',
+    )
+    priority = models.CharField(
+        max_length=1,
+        choices=PRIORITY_CHOICES,
+        default='1',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class FeatureRequest(models.Model):
+    STATUS_CHOICES = [
+        ('New', 'Рассмотрение'),
+        ('Yes', 'Принято'),
+        ('No', 'Отклонено'),
+    ]
+    PRIORITY_CHOICES = [
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE
+    )
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='New',
+    )
+    priority = models.CharField(
+        max_length=1,
+        choices=PRIORITY_CHOICES,
+        default='1',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
