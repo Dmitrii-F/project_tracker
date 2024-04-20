0351a7ac7acfacc9770c1be9ce05e20b9b0e2dad
Dmitrii <fedorovds@student.bmstu.ru>
2024-04-20 17:02:04 +0300

    Добавлены url. Для url project_tracker и quality_control четко разделены

--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
 
-# Create your views here.
+def index(request):
+    bug_list_url = reverse('control:bug_list')
+    feature_list_url = reverse('control:feature_list')
+    html = (f"<h1>Система контроля качества</h1>"
+            f"<a href='{bug_list_url}'>Список всех багов</a>"
+            f"<br/><a href='{feature_list_url}'>Запросы на улучшение</a>")
+    return HttpResponse(html)
+
+def bug_list(request):
+    return HttpResponse("Cписок отчетов об ошибках")
+
+def feature_list(request):
+    return HttpResponse("Список запросов на улучшение")
+
+def bug_detail(request, bug_id):
+    return HttpResponse(f"Детали бага {bug_id}")
+
+def feature_detail(request, feature_id):
+    return HttpResponse(f"Детали улучшения {feature_id}")
